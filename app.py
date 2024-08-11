8e390d8bb82243cfc4ef6c8b94ee89930db0991b
Austin Kennedy <austin25@illinois.edu>
2024-08-10 23:45:49 -0500

    first commit

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
-from flask import Flask, render_template
+from flask import Flask, jsonify
 
 app = Flask(__name__)
 
 @app.route('/')
-def index():
-    return render_template('index.html')
+def hello():
+    return jsonify(message="Hello, World!")
 
-if __name__ == '__main__':
-    app.run(debug=True)
+if __name__ == "__main__":
+    app.run()
